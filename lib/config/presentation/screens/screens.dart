77991877dc44bee23874e7867ed928ca2250d3d4
Matias López <alopez@eglobalt.com.py>
2024-02-16 15:51:14 -0300

    seccion terminada

--- lib/config/presentation/screens/screens.dart
+++ lib/config/presentation/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'package:flutter/material.dart';
+export 'package:widgets_app/config/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/config/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgets_app/config/presentation/screens/infinite_scroll/infinite_scroll.dart';
+export 'package:widgets_app/config/presentation/screens/progress/progress_screen.dart';
+
 export 'package:widgets_app/config/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/config/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/config/presentation/screens/home/home_screen.dart';
